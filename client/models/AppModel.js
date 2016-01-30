49655557924e6504a86bf1e21f745f85e65f6905
Daniel Wong <danielwong2268@gmail.com>
2016-01-30 01:33:59 -0800

    Complete enqueue functionality on library entry click

--- client/models/AppModel.js
+++ client/models/AppModel.js
@@ -1,6 +1,8 @@
 // App.js - Defines a backbone model class for the whole app.
 var AppModel = Backbone.Model.extend({
 
+  //library = new Songs(songData)
+  //params = library
   initialize: function(params) {
     this.set('currentSong', new SongModel());
     this.set('songQueue', new SongQueue());
@@ -12,9 +14,10 @@ var AppModel = Backbone.Model.extend({
     end up referring to the window. That's just what happens with all JS events. The handlers end up
     getting called from the window (unless we override it, as we do here). */
 
-
-    params.library.on('play', function(song) {
-      this.set('currentSong', song);
+    //library is the property
+    this.get('library').on('enqueue', function(song) {
+      this.get('songQueue').add(song);
+      console.log(this.get('songQueue'));
     }, this);
   }
 
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,10 +1,23 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
+  tagName: 'table',
+
   initialize: function() {
+    this.render();
+    this.collection.on('add', this.render,this);
   },
 
+
+
   render: function() {
+    console.log(this.collection);
+    this.$el.children().detach();
+    this.$el.html('<th>Song Queue</th>').append(
+      this.collection.map(function(song) {
+        return new SongQueueEntryView({model: song}).render();
+      }))
+    console.log('thisel',this.$el);
     return this.$el;
   }
 
